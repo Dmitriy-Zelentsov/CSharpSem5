1171f48d64b253f1ae24e14ecd3f1d81a8f9b953
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-18 21:40:38 +0300

    add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив. Напишите программу,
+//которая определяет, присутствует ли заданное
+//число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//-3; массив [6, 7, 19, 345, 3] -> да
+
+int [] CreateArray(int length, int min, int max)
+{
+    int [] array = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        array [i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+void FindNumber(int [] array)
+{
+    int num = Int32.Parse(Console.ReadLine());
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == num)5
+            Console.WriteLine("да");
+         Console.WriteLine("нет"); 
+         
+    }
+
+}
+int [] array = CreateArray(7, 3, 5);
+PrintArray(array);
+Console.WriteLine();
+FindNumber(array);
